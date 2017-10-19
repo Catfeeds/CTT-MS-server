f1673a9ec6d88dee550974d2bf55ac92b6963b4f
Horoll <640246255@qq.com>
2017-10-19 18:28:22 +0800

    2017-10-19 18:28

--- application/index/controller/Query.php
+++ application/index/controller/Query.php
@@ -44,29 +44,41 @@ class Query extends Base
        return json($list);
     }
 
+    //将查询到的数据数组转换为前端需要的格式
+    private function arrayHandel(array $arr){
+        $list = [];
+        foreach ($arr as $value){
+            $tmp = ['value'=>$value,'label'=>$value];
+            array_push($list,$tmp);
+        }
+        return $list;
+    }
+
     //查询某个地区所有的仓库
     public function storehouse($area){
-        return json(db('storehouse')->where('area',$area)->column('name'));
+        $storehouse = db('storehouse')->where('area',$area)->column('name');
+        $list = $this->arrayHandel($storehouse);
+        return json($list);
     }
 
     //查询某个地区所有的班组
     public function team($area){
-        return json(db('team')->where('area',$area)->column('name'));
+        $team = db('team')->where('area',$area)->column('name');
+        $list = $this->arrayHandel($team);
+        return json($list);
     }
 
     //查询所有的材料大类
     public function category(){
         $category = db('category')->where(1)->column('category_name');
-        $list = [];
-        foreach ($category as $value){
-            $tmp = ['value'=>$value,'label'=>$value];
-            array_push($list,$tmp);
-        }
+        $list = $this->arrayHandel($category);
         return json($list);
     }
 
     //根据材料大类返回材料名称
     public function stuff($category_name){
-        return json(db('stuff')->where('category_name',$category_name)->column('name'));
+        $stuff = db('stuff')->where('category_name',$category_name)->column('name');
+        $list = $this->arrayHandel($stuff);
+        return json($list);
     }
 }
